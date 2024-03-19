f1d947f4fda87619a8fb09f6d313fda86953cb0a
Olga Eremenko <olga.eremenko.1407@gmail.com>
2024-03-19 14:38:25 +0100

    Adding of three filters (homepage)

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -8,8 +8,41 @@ async function getWorks() {
     const gallery = document.querySelector(".gallery");
     
     data.forEach((workItem) => {
-        console.log(workItem.id);
+        console.log(workItem.title);
+        const figure = document.createElement("figure");
+        figure.innerHTML = `
+            <img src="${workItem.imageUrl}" alt="${workItem.title}">
+            <figcaption>${workItem.title}</figcaption>
+        `;
+        gallery.appendChild(figure);
     });
 }
 
 getWorks();
+
+async function getCategories() {
+    const response = await fetch("http://localhost:5678/api/categories");
+    const categories = await response.json();
+    // console.log(categories);
+
+    const menu = document.querySelector(".categories-menu");
+
+    categories.forEach((category) => {
+        console.log(category);
+        const filter = document.createElement("li");
+        filter.textContent = category.name;
+        filter.addEventListener("click", () => {
+            filterOfCategory(category.id);
+        });
+        menu.appendChild(filter);
+    });
+}
+
+async function filterOfCategory(categoryId) {
+    const response = await fetch("http://localhost:5678/api/works?category=${categoryId}");
+    const filterdWorks = await response.json();
+    // console.log(filterdWorks);
+
+}
+
+getCategories();
