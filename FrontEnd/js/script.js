124020807f0172005b65182407a4181978a5afbe
Olga Eremenko <olga.eremenko.1407@gmail.com>
2024-03-23 08:54:27 +0100

    Implemented functionality to filter works based on category selection

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -1,11 +1,23 @@
-async function getWorks() {
+async function getWorks(categoryId = null) {
     const response = await fetch("http://localhost:5678/api/works");
     const data = await response.json();
     // console.log(data); // [{id:..., title:"...3"...}]
 
     const gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
     
-    data.forEach((workItem) => {
+    data
+        .filter((work) => {
+            if (!categoryId) {
+                return true;
+            }
+            if (work.categoryId == categoryId) {
+                return true;
+            }
+            return false;
+        })
+    
+    .forEach((workItem) => {
         console.log(workItem.title);
         const figure = document.createElement("figure");
         figure.innerHTML = `
@@ -26,58 +38,35 @@ async function getCategories() {
             throw new Error("Error while retrieving categories");
         }
         const categories = await response.json();
-        console.log(categories);
 
         const allFilter = document.createElement("button");
-        allFilter.textContent = "Tous";
+        allFilter.textContent = "Tous"; // set the text content of allFilter button to "Tous"
         menu.appendChild(allFilter);
 
         categories.forEach((category) => {
             console.log(category);
             const filter = document.createElement("button");
-            filter.textContent = category.name;
+            filter.textContent = category.name; //  set the text content of the filter button to the name of the category
             filter.dataset.categoryId = category.id; // the attribute data-category-id to store the category ID
             menu.appendChild(filter);
         });
 
-        // Event listener for clicks on categories
-        menu.addEventListener("click", async (event) => {
-            let categoryId = null;
-            if (event.target.textContent !== "Tous") {
-                //Retrieve the category ID from the data attribute
-                categoryId = event.target.dataset.categoryId;
-            }
-            await filterOfCategory(categoryId);
-        });
+        handleClickCategoryButton();
     } catch (error) {
         console.error("An error occurred in getCategories:", error);
     }
 }
 
-async function filterOfCategory(categoryId) {
-    try {
-        const url = categoryId ? `http://localhost:5678/api/categories/${categoryId}/works` : "http://localhost:5678/api/works";
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error("Error while retrieving filtered works");
-        }
-        const filteredWorks = await response.json();
-        // console.log(filteredWorks);
-    
-        const gallery = document.querySelector(".gallery");
-        gallery.innerHTML = "";
-    
-        filteredWorks.forEach((workItem) => {
-            const figure = document.createElement("figure");
-            figure.innerHTML = `
-                <img src="${workItem.imageUrl}" alt="${workItem.title}">
-                <figcaption>${workItem.title}</figcaption>
-            `;
-            gallery.appendChild(figure);
+function handleClickCategoryButton() {
+    const buttons = document.querySelectorAll(".categories-menu button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", async (event) => {
+            const categoryId = event.target.dataset.categoryId;
+            //retirer active className sur tous les boutons
+            //className active sur button
+            getWorks(categoryId);
         });
-    } catch (error) {
-        console.error("An error occurred in filterOfCategory:", error);
-    }
+    });
 }
 
 getWorks();
